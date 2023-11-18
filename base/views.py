989fd7add231b28b30a89f8bd020091eb4f7851e
blaze <prabhatkumarverma42@gmail.com>
2023-11-18 21:07:09 +0530

    added login and logout views, added login and logout tempelate, updated main and navbar to add login and logout funcnalities

--- base/views.py
+++ base/views.py
@@ -1,8 +1,37 @@
 from django.shortcuts import redirect, render
 from django.db.models import Q
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 from .models import Room, Topic
 from .forms import RoomForm
+
 #Create your views here.
+
+def loginPage(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        try:
+            user = User.objects.get(username=username)
+        except:
+             messages.error(request, 'User does not exists')
+        
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect(home)
+        else:
+             messages.error(request, 'Username or Password does not exists')
+
+    context = {}
+    return render(request, 'base/login_register.html', context)
+
+def logoutUser(request):
+    logout(request)
+    return redirect('home')
 
 def home(request):
     q = request.GET.get('q') if request.GET.get('q') != None else '' #filter query
@@ -49,3 +78,4 @@ def deleteRoom(request, pk):
         room.delete()
         return redirect('home')
     return render(request, 'base/delete.html', {'obj':room})
+
