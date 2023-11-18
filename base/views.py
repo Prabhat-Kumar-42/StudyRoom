75d53077be522572ab8cf3fcf126709f5606647d
blaze <prabhatkumarverma42@gmail.com>
2023-11-18 15:04:01 +0530

    added Message, Room and Topic models, updates views relating to them, updated room template, added migrations

--- base/views.py
+++ base/views.py
@@ -1,23 +1,13 @@
 from django.shortcuts import render
-from django.http import request
-
+from .models import Room
 #Create your views here.
 
-rooms = [
-    {'id':1, 'name':'Room-1'},
-    {'id':2, 'name':'Room-2'},
-    {'id':3, 'name':'Room-3'},
-    {'id':4, 'name':'Room-4'},
-]
-
 def home(request):
+    rooms = Room.objects.all() 
     context = {'rooms': rooms}
     return render(request, 'base/home.html', context)
 
 def room(request, pk):
-    requested_room = None
-    for room in rooms:
-        if room['id'] == int(pk):
-            requested_room = room
+    requested_room = Room.objects.get(id=pk)
     context = {'room': requested_room}
     return render(request, 'base/room.html', context)
