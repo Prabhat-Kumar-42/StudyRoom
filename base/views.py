d896e9437e75360f5a108dbfe5d835c6b976ba8e
blaze <prabhatkumarverma42@gmail.com>
2023-11-18 23:47:58 +0530

    added register funcnalities in views, added the view url to urls.py, created templates for register in login_register.html, added option to show user name in navbar

--- base/views.py
+++ base/views.py
@@ -3,6 +3,7 @@ from django.http import response
 from django.db.models import Q
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from .models import Room, Topic
@@ -10,12 +11,33 @@ from .forms import RoomForm
 
 #Create your views here.
 
+def registerPage(request):
+    page = 'register'
+    form = UserCreationForm()
+
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.username = user.username.lower()
+            user.save()
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'Something went wrong')
+            
+
+    context = {'page':page, 'form': form}
+    return render(request, 'base/login_register.html', context)
+
 def loginPage(request):
+    page = 'login' 
+
     if request.user.is_authenticated:
         return redirect('home')
 
     if request.method == 'POST':
-        username = request.POST.get('username')
+        username = request.POST.get('username').lower()
         password = request.POST.get('password')
 
         try:
@@ -31,7 +53,7 @@ def loginPage(request):
         else:
              messages.error(request, 'Username or Password does not exists')
 
-    context = {}
+    context = {'page':page}
     return render(request, 'base/login_register.html', context)
 
 def logoutUser(request):
